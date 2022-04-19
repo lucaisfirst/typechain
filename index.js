765e19719ed51142f319f5fe4a36e69c0058fab3
김하진 <knj9417@gmail.com>
2022-04-20 02:05:41 +0900

    typescript

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "Luca", age = 21, gender = "male";
 const sayHello = (name, age, gender) => {
-    console.log(`Hello ${name}, you are a ${age}, you are a ${gender}`);
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
 };
 sayHello(name, age);
 //# sourceMappingURL=index.js.map
