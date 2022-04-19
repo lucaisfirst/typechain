50dd39eda5e21b0dfa18a45a33339801d37689a6
김하진 <knj9417@gmail.com>
2022-04-19 23:58:52 +0900

    typescript compile

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Luca", age = 21, gender = "male";
+const sayHello = (name, age, gender) => {
+    console.log(`Hello ${name}, you are a ${age}, you are a ${gender}`);
+};
+sayHello(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello")
+const name = "Luca",
+    age = 21,
+    gender = "male";
+
+const sayHello = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHello(name, age);
+
+export {};
